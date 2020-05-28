f533d2203f1d90d0c888a8a574339b5a4fc48b46
Mafrans <malte.kluft@gmail.com>
2020-05-28 14:19:04 +0200

    Add button component

---
+++ src/components/Button.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="button">
+        <p><slot></slot></p>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped lang="scss">
+@use "../colors.scss";
+@use "../text.scss";
+
+.button {
+    padding: 25px 70px;
+    font-style: normal;
+    font-weight: 600;
+    font-size: 24px;
+    line-height: 29px;
+    background: colors.$magenta;
+    display: inline-block;
+    border-radius: 50px;
+    margin-top: 2rem;
+
+    cursor: pointer;
+}
+</style>
