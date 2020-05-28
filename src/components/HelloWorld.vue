159e0c45ee27ebd4590f6be35e6cbd6d9e69ee13
Mafrans <malte.kluft@gmail.com>
2020-05-28 14:18:17 +0200

    Hello World Page

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,24 +1,34 @@
 <template>
-  <div class="hello-world">
-      <img class="shape1" src="../assets/Shape 1.svg" alt="">
+    <div class="hello-world">
+        <img class="shape shape1" src="../assets/Shape 1.svg" alt="">
 
-      <div class="text-container">
-          <h1>Hello <span class="purple">World</span></h1>
-          <p>
+        <div class="text-container">
+            <h1>Hello <span class="purple">World</span></h1>
+            <p>
                 I’m a young web designer and front end developer, you probably know me from Stadia+ or Poppobot. <br>
                 <br>
                 Have something to say?
-          </p>
-      </div>
-  </div>
+            </p>
+            <btn>Contact Me</btn>
+        </div>
+        
+        <div class="profile">
+            <img class="shape shape2" src="../assets/Shape 2.svg" alt="">
+            <div class="circle1"></div>
+            <div class="circle2"></div>
+            <img src="../assets/profile picture.png" alt="" class="profile-picture">
+        </div>
+    </div>
 </template>
 
-<script lang="ts">
+<script>
 import { Component, Prop, Vue } from 'vue-property-decorator';
+import Button from "./Button.vue";
 
-@Component
-export default class HelloWorld extends Vue {
-  @Prop() private msg!: string;
+export default {
+    components: {
+        btn: Button,
+    }
 }
 </script>
 
@@ -27,10 +37,24 @@ export default class HelloWorld extends Vue {
 @use '../colors.scss';
 @use '../text.scss';
 
-.shape1 {
+.hello-world {
+    height: 100vh;
+    width: 100vw;
+}
+
+.shape {
+    pointer-events: none;
     position: absolute;
-    top: 0;
-    left: 0;
+
+    &.shape1 {
+        top: 0;
+        left: 0;
+    }
+
+    &.shape2 {
+        top: 10%;
+        right: 3%;
+    }
 }
 
 .text-container {
@@ -46,6 +70,36 @@ export default class HelloWorld extends Vue {
 
     .purple {
         color: colors.$purple;
+    }
+}
+
+.profile {
+    .profile-picture {
+        position: absolute;
+        top: 25%;
+        right: 10.5%;
+        width: 25%;
+        border-radius: 50%;
+    }
+
+    .circle2 {
+        position: absolute;
+        top: 22%;
+        right: 8%;
+        width: 22vw;
+        height: 22vw;
+        border-radius: 50%;
+        background: colors.$pink;
+    }
+
+    .circle1 {
+        position: absolute;
+        top: 21%;
+        right: 7%;
+        width: 15vw;
+        height: 15vw;
+        border-radius: 50%;
+        background: colors.$light-pink;
     }
 }
 </style>
